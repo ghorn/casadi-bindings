24ea304b5b6cbc16aaed942d8f51402b35eedb46
Joris Gillis <joris.gillis42@gmail.com>
2013-09-26 12:55:40 +0200

    Marshalling: fixed all compilation errors of the C++ generated file

--- cbits/marshall.cpp
+++ cbits/marshall.cpp
@@ -3,17 +3,3 @@
 
 using namespace std;
 
-string marshall(char * x){ return string(x); }
-int marshall(int x){ return x; }
-double marshall(double x){ return x; }
-const CasADi::MX& marshall(const CasADi::MX& x){ return x; }
-
-vector<CasADi::SXMatrix> marshall(vector<CasADi::SXMatrix*> const & inputs){
-    vector<CasADi::SXMatrix> vec;
-    for (unsigned int k=0; k<inputs.size(); k++){
-        vec.push_back( *(inputs[k]) );
-    }
-    return vec;
-}
-
-
