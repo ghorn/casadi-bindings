388b7f46a304c9ce37c3714314a3bbd6fff89123
Greg Horn <gregmainland@gmail.com>
2013-09-30 16:39:42 +0200

    fix test with new camelCase names, and print meta stuff in test

--- test.hs
+++ test.hs
@@ -9,14 +9,41 @@ import Casadi.Wrappers.SharedObject
 import Casadi.Wrappers.IOInterfaceFX
 import Casadi.Wrappers.FX
 import Casadi.Wrappers.PrintableObject
+import Casadi.Wrappers.CasadiMeta
 
 main :: IO ()
 main = do
+  putStr "casadiMeta_getBuildType: "
+  casadiMeta_getBuildType >>= putStrLn
+
+  putStr "casadiMeta_getCompiler: "
+  casadiMeta_getCompiler >>= putStrLn
+
+  putStr "casadiMeta_getCompilerFlags: "
+  casadiMeta_getCompilerFlags >>= putStrLn
+
+  putStr "casadiMeta_getCompilerId: "
+  casadiMeta_getCompilerId >>= putStrLn
+
+  putStr "casadiMeta_getFeatureList: "
+  casadiMeta_getFeatureList >>= putStrLn
+
+  putStr "casadiMeta_getGitDescribe: "
+  casadiMeta_getGitDescribe >>= putStrLn
+
+  putStr "casadiMeta_getGitRevision: "
+  casadiMeta_getGitRevision >>= putStrLn
+
+  putStr "casadiMeta_getVersion: "
+  casadiMeta_getVersion >>= putStrLn
+
+  putStrLn "---------------------------"
   x <- ssym'' "x"
-  f <- sXFunction''' (V.fromList [x]) (V.fromList [x])
+  f <- sxFunction''' (V.fromList [x]) (V.fromList [x])
   sharedObject_init' f
-  iOInterfaceFX_setInput''' f (V.fromList [3]) 0
-  fX_evaluate'' f
-  out <- iOInterfaceFX_output f 0
+  ioInterfaceFX_setInput''' f (V.fromList [3]) 0
+  fx_evaluate'' f
+  out <- ioInterfaceFX_output f 0
   printableObject_getDescription out >>= putStrLn
   printableObject_getRepresentation out >>= putStrLn
+
