9fba1ab2509b56ac8b16db7e862827a92a4ff21f
Greg Horn <gregmainland@gmail.com>
2013-09-30 15:15:53 +0200

    get libraries (and their order) of extra-libraries from casadi cmake, test scrip now works!!!!!

--- test.hs
+++ test.hs
@@ -6,10 +6,17 @@ import qualified Data.Vector as V
 import Casadi.Wrappers.SXFunction
 import Casadi.Wrappers.Tools
 import Casadi.Wrappers.SharedObject
+import Casadi.Wrappers.IOInterfaceFX
+import Casadi.Wrappers.FX
+import Casadi.Wrappers.PrintableObject
 
 main :: IO ()
 main = do
   x <- ssym'' "x"
   f <- sXFunction''' (V.fromList [x]) (V.fromList [x])
   sharedObject_init' f
-  return ()
+  iOInterfaceFX_setInput''' f (V.fromList [3]) 0
+  fX_evaluate'' f
+  out <- iOInterfaceFX_output f 0
+  printableObject_getDescription out >>= putStrLn
+  printableObject_getRepresentation out >>= putStrLn
