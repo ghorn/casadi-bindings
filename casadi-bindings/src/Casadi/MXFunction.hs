c3448f3467510bf442b098bda2c5d73718723208
Greg Horn <gregmainland@gmail.com>
2014-10-22 17:11:34 +0200

    add some Function/SXFunction/MXFunction converters

--- casadi-bindings/src/Casadi/MXFunction.hs
+++ casadi-bindings/src/Casadi/MXFunction.hs
@@ -1,13 +1,19 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Casadi.MXFunction
-       ( C.MXFunction, mxFunction
+       ( C.MXFunction
+       , mxFunction
+       , mxFunctionFromFunction
        ) where
 
 import Data.Vector ( Vector )
 
+import qualified Casadi.Core.Classes.Function as C
 import qualified Casadi.Core.Classes.MXFunction as C
 import Casadi.MX ( MX )
 
 mxFunction :: Vector MX -> Vector MX -> IO C.MXFunction
 mxFunction = C.mxFunction__0
+
+mxFunctionFromFunction :: C.Function -> IO C.MXFunction
+mxFunctionFromFunction = C.mxFunction__1
--- casadi-bindings/src/Casadi/SXFunction.hs
+++ casadi-bindings/src/Casadi/SXFunction.hs
@@ -1,13 +1,24 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Casadi.SXFunction
-       ( C.SXFunction, sxFunction
+       ( C.SXFunction
+       , sxFunction
+       , sxFunctionFromFunction
+       , sxFunctionFromMXFunction
        ) where
 
 import Data.Vector ( Vector )
 
+import qualified Casadi.Core.Classes.Function as C
 import qualified Casadi.Core.Classes.SXFunction as C
+import qualified Casadi.Core.Classes.MXFunction as C
 import Casadi.SX ( SX )
 
 sxFunction :: Vector SX -> Vector SX -> IO C.SXFunction
 sxFunction = C.sxFunction__0
+
+sxFunctionFromFunction :: C.Function -> IO C.SXFunction
+sxFunctionFromFunction = C.sxFunction__1
+
+sxFunctionFromMXFunction :: C.MXFunction -> IO C.SXFunction
+sxFunctionFromMXFunction = C.sxFunction__2
