02db4e455feba6950b6fb2befbbbfc82109fb22b
Greg Horn <gregmainland@gmail.com>
2014-12-03 14:57:57 +0100

    add CustomFunction / DerivativeGenerator

--- casadi-bindings-internal/src/Casadi/Internal/Callback.hs
+++ casadi-bindings-internal/src/Casadi/Internal/Callback.hs
@@ -5,6 +5,10 @@
 module Casadi.Internal.Callback
        ( c_newCallbackHaskell
        , mkCallback
+       , c_newCustomEvaluateHaskell
+       , mkCustomEvaluate
+       , c_newDerivativeGeneratorHaskell
+       , mkDerivativeGenerator
        ) where
 
 
@@ -16,8 +20,24 @@ import Foreign.Ptr ( Ptr, FunPtr )
 --import Casadi.Internal.Marshal ( withMarshal )
 --import Casadi.Internal.WrapReturn ( WrapReturn(..) )
 
-type CasadiCallback' function' = Ptr function' -> IO CInt
-foreign import ccall "wrapper" mkCallback :: CasadiCallback' function' -> IO (FunPtr (CasadiCallback' function'))
+-- these guys all have type parameters because core depends on internal, not the other way around
 
+type CasadiCallback' function = Ptr function -> IO CInt
+foreign import ccall "wrapper" mkCallback
+  :: CasadiCallback' function -> IO (FunPtr (CasadiCallback' function))
 foreign import ccall safe "new_callback_haskell" c_newCallbackHaskell
-  :: FunPtr (CasadiCallback' function') -> IO (Ptr callback')
+  :: FunPtr (CasadiCallback' function) -> IO (Ptr callback)
+
+
+type CasadiCustomEvaluate' customFunction = Ptr customFunction -> IO ()
+foreign import ccall "wrapper" mkCustomEvaluate
+  :: CasadiCustomEvaluate' customFunction -> IO (FunPtr (CasadiCustomEvaluate' customFunction))
+foreign import ccall safe "new_custom_evaluate_haskell" c_newCustomEvaluateHaskell
+  :: FunPtr (CasadiCustomEvaluate' customFunction) -> IO (Ptr customEvaluate)
+
+
+type CasadiDerivativeGenerator' function = Ptr function -> CInt -> CInt -> IO (Ptr function)
+foreign import ccall "wrapper" mkDerivativeGenerator
+  :: CasadiDerivativeGenerator' function -> IO (FunPtr (CasadiDerivativeGenerator' function))
+foreign import ccall safe "new_derivative_generator_haskell" c_newDerivativeGeneratorHaskell
+  :: FunPtr (CasadiDerivativeGenerator' function) -> IO (Ptr derivativeGenerator)
--- casadi-bindings/src/Casadi/Callback.hs
+++ casadi-bindings/src/Casadi/Callback.hs
@@ -4,15 +4,21 @@
 
 module Casadi.Callback
        ( makeCallback
+       , makeCustomEvaluate
+       , makeDerivativeGenerator
        ) where
 
-import Foreign.C.Types
+import Foreign.C.Types ( CInt )
 import Foreign.Ptr ( Ptr )
 import Foreign.ForeignPtr ( newForeignPtr_ )
+import Foreign.ForeignPtr.Unsafe ( unsafeForeignPtrToPtr )
 
 import Casadi.Core.Data
 import Casadi.Internal.WrapReturn ( WrapReturn(..) )
-import Casadi.Internal.Callback ( mkCallback, c_newCallbackHaskell )
+import Casadi.Internal.Callback ( mkCallback, c_newCallbackHaskell
+                                , mkCustomEvaluate, c_newCustomEvaluateHaskell
+                                , mkDerivativeGenerator, c_newDerivativeGeneratorHaskell
+                                )
 
 -- | add a callback to an NLPSolver
 makeCallback :: (Function -> IO CInt) -> IO Callback
@@ -28,3 +34,36 @@ makeCallback callback = do
 
   -- create the callback object
   (c_newCallbackHaskell callbackFunPtr :: IO (Ptr Callback')) >>= wrapReturn
+
+
+-- | add a callback to an NLPSolver
+makeCustomEvaluate :: (CustomFunction -> IO ()) -> IO CustomEvaluate
+makeCustomEvaluate callback = do
+  -- safely wrap the callback into the C-friendly version
+  let callback' :: Ptr CustomFunction' -> IO ()
+      callback' ptrFx = do
+        foreignCFun <- newForeignPtr_ ptrFx
+        callback (CustomFunction foreignCFun)
+
+  -- turn the callback into a FunPtr
+  callbackFunPtr <- mkCustomEvaluate callback'
+
+  -- create the callback object
+  (c_newCustomEvaluateHaskell callbackFunPtr :: IO (Ptr CustomEvaluate')) >>= wrapReturn
+
+
+-- | add a callback to an NLPSolver
+makeDerivativeGenerator :: (Function -> Int -> Int -> IO Function) -> IO DerivativeGenerator
+makeDerivativeGenerator callback = do
+  -- safely wrap the callback into the C-friendly version
+  let callback' :: Ptr Function' -> CInt -> CInt -> IO (Ptr Function')
+      callback' ptrFx nfwd nadj = do
+        foreignCFun <- newForeignPtr_ ptrFx
+        Function fun <- callback (Function foreignCFun) (fromIntegral nfwd) (fromIntegral nadj)
+        return (unsafeForeignPtrToPtr fun)
+
+  -- turn the callback into a FunPtr
+  callbackFunPtr <- mkDerivativeGenerator callback'
+
+  -- create the callback object
+  (c_newDerivativeGeneratorHaskell callbackFunPtr :: IO (Ptr DerivativeGenerator')) >>= wrapReturn
