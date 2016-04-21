659c5b6b86d5133b4c5e2553abfeeb8113bb431c
Greg Horn <gregmainland@gmail.com>
2016-04-21 14:09:56 -0700

    add extern "C"

--- casadi-bindings/cbits/handwritten.cpp
+++ casadi-bindings/cbits/handwritten.cpp
@@ -1,11 +1,11 @@
 #include "math.h"
 
-double c_fmod(double x, double y);
+extern "C" double c_fmod(double x, double y);
 double c_fmod(double x, double y) {
   return fmod(x, y);
 }
 
-float c_fmodf(float x, float y);
+extern "C" float c_fmodf(float x, float y);
 float c_fmodf(float x, float y) {
   return fmodf(x, y);
 }
