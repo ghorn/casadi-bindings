bd4a40a512686d9356a36ba7b1263c9629b6c92e
Greg Horn <gregmainland@gmail.com>
2016-04-21 14:09:04 -0700

    add prototypes

--- casadi-bindings/cbits/handwritten.cpp
+++ casadi-bindings/cbits/handwritten.cpp
@@ -1,9 +1,11 @@
 #include "math.h"
 
+double c_fmod(double x, double y);
 double c_fmod(double x, double y) {
   return fmod(x, y);
 }
 
+float c_fmodf(float x, float y);
 float c_fmodf(float x, float y) {
   return fmodf(x, y);
 }
